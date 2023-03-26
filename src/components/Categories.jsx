c6a009a8198801009e2bdcdd96a47ea392c36d10
Lancet03 <ilja.efremov-2003@mail.ru>
2023-03-26 12:01:21 +0400

    pizzas states

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,38 @@
+import React from 'react';
+
 function Categories() {
+    const [categoryIndex, setCategoryIndex] = React.useState(0);
+
+    const categories = ["Все",
+        "Мясные",
+        "Вегетарианские",
+        "Гриль",
+        "Острые",
+        "Закрытые"];
+
+    const onCategory = (index) => {
+        setCategoryIndex(index);
+
+    };
+
     return (
-        <div class="categories">
+        <div className="categories">
             <ul>
-                <li class="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {categories.map(
+                    (category, index) => (
+                        <li
+                            key={index}
+                            onClick={() => onCategory(index)}
+                            className={categoryIndex === index ? "active" : ""}
+                        >
+                            {category}
+                        </li>)
+                )}
+                {/* <li>Мясные</li>
+         <li>Вегетарианская</li>
+         <li>Гриль</li>
+         <li>Острые</li>
+         <li>Закрытые</li> */}
             </ul>
         </div>
     )
